f643a943f7377425d249b07fa75a446fd51f198e
user <katya.rotar02@gmail.com>
2023-11-14 10:25:52 +0200

    Task_1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<Student> students = new List<Student>();
+        while (true)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+            if (input[0].ToLower() == "end") {
+                break;
+            }
+            string firstName = input[0] + " " + input[1];
+            int group = int.Parse(input[2]);
+            Student student = new Student(firstName, group);
+            students.Add(student);
+        }
+        var output = from student in students
+                     where student.Group == 2
+                     orderby student.FirstName
+                     select student;
+        foreach(var student in output) {
+            Console.WriteLine(student);
+        }
+    }
+}
---
+++ Task_1/Student.cs
@@ -0,0 +1,12 @@
+﻿class Student{
+    public string FirstName { get; set; }
+    public int Group {  get; set; }
+    public Student(string firstName, int group) {
+        FirstName = firstName;
+        Group = group;
+    }
+    public override string ToString()
+    {
+        return $"{FirstName} {Group}";
+    }
+}
