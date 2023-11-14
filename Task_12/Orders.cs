b4eb89139f45f7318b10f81921ef9c65cf90e764
user <katya.rotar02@gmail.com>
2023-11-14 21:03:17 +0200

    Task_12

---
+++ Task_12/Orders.cs
@@ -0,0 +1,11 @@
+﻿class Orders{
+    public string Company {  get; set; }
+    public int Amount { get; set; }
+    public string Product {  get; set; }
+    public Orders(string company, int amount, string product)
+    {
+        Company = company;
+        Amount = amount;
+        Product = product;
+    }
+}
---
+++ Task_12/Program.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        List<Orders> orders = new List<Orders>();
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            string[] input = Console.ReadLine().Split(" - ");
+            string company = input[0];
+            int amount = int.Parse(input[1]);
+            string product = input[2];
+            Orders order = new Orders(company, amount, product);
+            orders.Add(order);
+        }
+        var output = orders.GroupBy(g => g.Company)
+                           .OrderBy(o => o.Key);
+        foreach (var order_2 in output)
+        {
+            Console.Write($"{order_2.Key}: ");
+            var product = order_2.GroupBy(g => g.Product)
+                                 .Select(s => $"{s.Key}-{s.Sum(a => a.Amount)}");
+            Console.WriteLine(string.Join(", ", product));
+        }
+    }
+}
